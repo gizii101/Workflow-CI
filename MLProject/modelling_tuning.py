92364c5abd6cd49d0f8e54da36faafc255a6f5b3
gizii101 <155464512+gizii101@users.noreply.github.com>
2025-12-24 09:30:36 +0700

    Update modelling_tuning.py

--- MLProject/modelling_tuning.py
+++ MLProject/modelling_tuning.py
@@ -1,15 +1,17 @@
 import pandas as pd
 import mlflow
 import mlflow.sklearn
+import json
+import os
+import joblib
+
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
 
 def main():
-    # PENTING: jangan pakai start_run()
-    mlflow.set_experiment("Heart_Disease_Classification")
 
-    # Data HARUS relatif ke MLProject/
     df = pd.read_csv("heart_preprocessing.csv")
 
     X = df.drop(columns=["HeartDisease"])
@@ -19,24 +21,56 @@ def main():
         X, y, test_size=0.2, random_state=42, stratify=y
     )
 
-    model = RandomForestClassifier(
-        n_estimators=200,
-        max_depth=10,
-        random_state=42
+    param_grid = {
+        "n_estimators": [100, 200],
+        "max_depth": [None, 10, 20],
+        "min_samples_split": [2, 5],
+        "criterion": ["gini", "entropy"]
+    }
+
+    model = RandomForestClassifier(random_state=42)
+
+    grid = GridSearchCV(
+        model,
+        param_grid,
+        cv=5,
+        scoring="accuracy",
+        n_jobs=-1,
+        verbose=1
     )
 
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
+    grid.fit(X_train, y_train)
+
+    best_model = grid.best_estimator_
+    y_pred = best_model.predict(X_test)
 
     acc = accuracy_score(y_test, y_pred)
+    prec = precision_score(y_test, y_pred)
+    rec = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
 
+    mlflow.log_params(grid.best_params_)
     mlflow.log_metric("accuracy", acc)
+    mlflow.log_metric("precision", prec)
+    mlflow.log_metric("recall", rec)
+    mlflow.log_metric("f1_score", f1)
 
-    # ⬇️ INI KUNCI UTAMA
+    # ⬇️ INI PENTING BUAT DOCKER
+    os.makedirs("random_forest_model", exist_ok=True)
+    joblib.dump(best_model, "random_forest_model/model.pkl")
+
     mlflow.sklearn.log_model(
-        sk_model=model,
-        artifact_path="model"
+        sk_model=best_model,
+        artifact_path="random_forest_model"
     )
+
+    with open("performance_report.txt", "w") as f:
+        f.write(f"Accuracy: {acc}\n")
+
+    mlflow.log_artifact("performance_report.txt")
+
+    print("TRAINING SELESAI")
+
 
 if __name__ == "__main__":
     main()
